721229efb2bbe75b838dc2cab872cf3756ee545a
josmoss <josmoss@gmail.com>
2016-07-06 15:24:16 -0600

    Dog model class. Table view delegate methods.

---
+++ PetFiinder/PetFiinder/Dog.swift
@@ -0,0 +1,24 @@
+//
+//  Dog.swift
+//  PetFiinder
+//
+//  Created by Joe Moss on 7/6/16.
+//  Copyright © 2016 Iron Yard_Joe Moss. All rights reserved.
+//
+
+import UIKit
+
+class Dog: NSObject {
+    
+    var name : String = ""
+    var age : Int = 0
+    var imageName : String = ""
+    var breed : String = ""
+    var size : String = ""
+    var gender : String = ""
+    
+    override init() {
+        super.init()
+    }
+
+}
